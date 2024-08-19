63f01425f4ec1e7176ef5e276d3940c3628f51a8
valeskaYcp <valeska.condori@tecsup.edu.pe>
2024-08-18 20:34:10 -0500

    first commit

---
+++ src/Acuario/Acuario.kt
@@ -0,0 +1,52 @@
+package Acuario
+
+import kotlin.math.PI
+
+open class Acuario(open var largo: Int = 100, open var ancho: Int = 20, open var alto: Int = 40) {
+
+    open var volumen: Int
+        get() = ancho * alto * largo / 1000  // 1000 cm^3 = 1 l
+        set(valor) {
+            alto = (valor * 1000) / (ancho * largo)
+        }
+
+    open val forma: String = "rectángulo"
+
+    open val agua: Double
+        get() = volumen * 0.9
+
+    fun imprimirTamano() {
+        println(forma)
+        println("Ancho: $ancho cm " +
+                "Largo: $largo cm " +
+                "Alto: $alto cm ")
+        println("Volumen: $volumen l Agua: $agua l (${agua/volumen*100.0}% lleno)")
+    }
+
+    constructor(numeroDePeces: Int) : this() {
+        // 2,000 cm^3 por pez + espacio extra para que no se derrame
+        val tanque = numeroDePeces * 2000 * 1.1
+        alto = (tanque / (largo * ancho)).toInt()
+    }
+}
+
+class TanqueTorre (override var alto: Int, var diametro: Int):
+    Acuario(alto = alto, ancho = diametro, largo = diametro) {
+    override var volumen: Int
+        // área elíptica = π * r1 * r2
+        get() = (ancho/2 * largo/2 * alto / 1000 * PI).toInt()
+        set(valor) {
+            alto = ((valor * 1000 / PI) / (ancho/2 * largo/2)).toInt()
+        }
+
+    override var agua = volumen * 0.8
+    override val forma = "cilindro"
+}
+
+
+
+
+
+
+
+
---
+++ src/Acuario/main.kt
@@ -0,0 +1,34 @@
+package Acuario
+
+fun construirAcuario() {
+    /* val acuario1 = Acuario()
+    acuario1.imprimirTamano()
+    // altura y largo por defecto
+    val acuario2 = Acuario(ancho = 25)
+    acuario2.imprimirTamano()
+    // ancho por defecto
+    val acuario3 = Acuario(alto = 35, largo = 110)
+    acuario3.imprimirTamano()
+    // todo personalizado
+    val acuario4 = Acuario(ancho = 25, alto = 35, largo = 110)
+    acuario4.imprimirTamano()
+    val acuario6 = Acuario(numeroDePeces = 29)
+    acuario6.imprimirTamano()
+    val acuario7 = Acuario(numeroDePeces = 29)
+    acuario7.imprimirTamano()
+    acuario7.volumen = 70
+    acuario7.imprimirTamano()
+    */
+
+    val miAcuario = Acuario(ancho = 25, largo = 25, alto = 40)
+    miAcuario.imprimirTamano()
+    println()
+
+    val miTorre = TanqueTorre(diametro = 25, alto = 40)
+    miTorre.imprimirTamano()
+}
+
+fun main() {
+    construirAcuario()
+}
+
